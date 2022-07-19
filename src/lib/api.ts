616a8515b6519c947bdd7603efe3c7aa43ed8b1e
seasnail <mcwalterarlo@gmail.com>
2022-07-19 13:53:59 +0100

    make the api base configurable

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,7 +11,7 @@ export function api(path: string, auth = false, method = "GET"): Promise<any> {
             Authorization: "Bearer " + get(token)
         };
 
-        fetch("https://meteorclient.com/api/" + path, options)
+        fetch(import.meta.env.API_BASE + path, options)
             .then(rawRes => {
                 rawRes.json().then(res => {
                     if (rawRes.status == 200) resolve(res);
