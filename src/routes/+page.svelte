6a19367f6af61de339fb6bfb3803862947448f85
Talhoid <68508139+Talhoid@users.noreply.github.com>
2023-04-01 06:40:48 -0400

    remove mpvp from main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import Navbar from "$lib/components/navbar.svelte";
     import Main from "$lib/components/main.svelte";
     import Info from "$lib/components/info.svelte";
-    import Mpvp from "$lib/components/mpvp.svelte";
     import Team from "$lib/components/team.svelte";
     import Footer from "$lib/components/footer.svelte";
 </script>
@@ -10,6 +9,5 @@
 <Navbar/>
 <Main/>
 <Info stats={$$props.data.stats}/>
-<Mpvp/>
 <Team/>
 <Footer/>
