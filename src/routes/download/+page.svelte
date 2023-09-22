7ec0f2b70abe705f2471ff402bc1896905834e8c
MineGame159 <petulko08@gmail.com>
2023-09-22 20:52:35 +0200

    Trying out google adsense instead of adfocus

---
+++ src/routes/download/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import { onMount } from "svelte";
+
+    let timer = 5;
+
+    onMount(() => {
+        const w: any = window;
+        (w.adsbygoogle = w.adsbygoogle || []).push({});
+
+        setInterval(() => {
+            timer--;
+
+            if (timer <= 0) {
+                if ($page.url.searchParams.has("devBuild"))
+                    goto("/api/download?devBuild=latest");
+                else
+                    goto("/api/download");
+            }
+        }, 1000);
+    });
+</script>
+
+<svelte:head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8546584413187411"crossorigin="anonymous"></script>
+</svelte:head>
+
+{#if browser}
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-8546584413187411"
+        data-ad-slot="5754111941"
+        data-ad-format="auto"
+        data-full-width-responsive="true">
+    </ins>
+{/if}
+
+<p>Download will start in {timer} seconds</p>
+
+{#if browser}
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-8546584413187411"
+        data-ad-slot="5754111941"
+        data-ad-format="auto"
+        data-full-width-responsive="true">
+    </ins>
+{/if}
+
+<style>
+    p {
+        margin: 4rem;
+        text-align: center;
+    }
+</style>
--- src/routes/download/+server.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async ({ url }) => {
-    let newUrl = new URL("https://meteorclient.com/api/download");
-    url.searchParams.forEach((value, key) => newUrl.searchParams.set(key, value));
-    return await fetch(newUrl);
-};
