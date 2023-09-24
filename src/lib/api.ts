c36c005147f8b58dff175881e8f6c25f42cb6318
Ollie <numonegamings@gmail.com>
2023-09-24 15:27:00 +0100

    Update api.ts
    Included better error handling and used async/await rather than promise chaining with .then()

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,36 +1,50 @@
 import { get } from "svelte/store";
 import { token } from "./user";
 
-export function api(path: string, auth = false, method = "GET", body: BodyInit | null = null): Promise<any> {
-    return new Promise((resolve, reject) => {
-        let options: RequestInit = {
-            method: method,
-            body: body
-        };
+export async function api(
+  path: string,
+  auth = false,
+  method = "GET",
+  body: BodyInit | null = null
+): Promise<any> {
+  try {
+    const options: RequestInit = {
+      method,
+      body,
+    };
 
-        if (auth) options.headers = {
-            Authorization: "Bearer " + get(token)
-        };
+    if (auth) {
+      options.headers = {
+        Authorization: "Bearer " + get(token),
+      };
+    }
 
-        fetch("https://meteorclient.com/api/" + path, options)
-            .then(rawRes => {
-                rawRes.json().then(res => {
-                    if (rawRes.status == 200) resolve(res);
-                    else reject(res.error);
-                });
-            })
-            .catch(reason => reject(reason));
-    });
+    const response = await fetch("https://meteorclient.com/api/" + path, options);
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.error);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("API request failed: " + error.message);
+  }
 }
 
-export function fetchJson(url: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(res => {
-                res.json()
-                    .then(json => resolve(json))
-                    .catch(reason => reject(reason));
-            })
-            .catch(reason => reject(reason));
-    });
+export async function fetchJson(url: string): Promise<any> {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.error);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("JSON fetch failed: " + error.message);
+  }
 }
