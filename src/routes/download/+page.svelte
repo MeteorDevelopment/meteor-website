732048995607ca08ae9a1eb94769cae14637274b
MineGame159 <petulko08@gmail.com>
2023-10-05 15:12:26 +0200

    Improve download page

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -9,8 +9,9 @@
     onMount(() => {
         const w: any = window;
         (w.adsbygoogle = w.adsbygoogle || []).push({});
+        (w.adsbygoogle = w.adsbygoogle || []).push({});
 
-        setInterval(() => {
+        const interval = setInterval(() => {
             timer--;
 
             if (timer == 0) {
@@ -18,6 +19,8 @@
                     goto("/api/download?devBuild=latest");
                 else
                     goto("/api/download");
+
+                clearInterval(interval);
             }
         }, 1000);
     });
@@ -27,31 +30,43 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8546584413187411"crossorigin="anonymous"></script>
 </svelte:head>
 
-{#if browser}
-    <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-8546584413187411"
-        data-ad-slot="5754111941"
-        data-ad-format="auto"
-        data-full-width-responsive="true">
-    </ins>
-{/if}
+<div>
+    {#if browser}
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-8546584413187411"
+            data-ad-slot="5754111941"
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
+    {/if}
 
-<p>Download will start in {timer} seconds</p>
+    {#if timer > 1}
+        <p>Download will start in {timer} seconds</p>
+    {:else if timer > 0}
+        <p>Download will start in 1 second</p>
+    {:else}
+        <p>Download started</p>
+    {/if}
 
-{#if browser}
-    <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-8546584413187411"
-        data-ad-slot="5754111941"
-        data-ad-format="auto"
-        data-full-width-responsive="true">
-    </ins>
-{/if}
+    {#if browser}
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-8546584413187411"
+            data-ad-slot="5754111941"
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
+    {/if}
+</div>
 
 <style>
-    p {
-        margin: 4rem;
+    div {
+        margin: 2rem;
         text-align: center;
+    }
+
+    p {
+        margin: 2rem 0;
     }
 </style>
