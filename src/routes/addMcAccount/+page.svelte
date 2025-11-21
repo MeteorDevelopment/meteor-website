6e2940605a71a16eb8099b56451a09f647ad31ba
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2025-11-21 13:44:35 +0100

    Replace OTP input component with up to date bits-ui

--- src/routes/addMcAccount/+page.svelte
+++ src/routes/addMcAccount/+page.svelte
@@ -3,7 +3,8 @@
     import { goto } from "$app/navigation"
     import { refreshUser, token } from "$lib/user"
     import Navbar from "$lib/components/navbar.svelte"
-    import { OTPInput, OTPRoot } from "@jimmyverburgt/svelte-input-otp"
+    // noinspection ES6UnusedImports
+    import { PinInput, REGEXP_ONLY_DIGITS } from "bits-ui"
     import { api } from "$lib/api"
 
     $effect(() => {
@@ -13,10 +14,10 @@
     let value = $state<string>("")
     let errorMessage = $state<string>("")
 
-    function onComplete(code: string) {
+    function onComplete() {
         errorMessage = ""
 
-        api("account/mcAccount?code=" + code, true, "POST")
+        api("account/mcAccount?code=" + value, true, "POST")
             .then(() => {
                 refreshUser()
                 goto("/account")
@@ -28,26 +29,33 @@
     }
 </script>
 
-<Navbar />
+<Navbar/>
 
 <div class="container">
     <div>
         <h1>Add MC account</h1>
 
         <ol>
-            <li>Launch your Minecraft client and join our server with the IP <span class="ip">mcauth.meteorclient.com</span></li>
+            <li>Launch your Minecraft client and join our server with the IP <span
+                class="ip">mcauth.meteorclient.com</span></li>
             <li>You will be immediately kicked with a one-time 6-digit code</li>
             <li>Write the code into the input box below</li>
         </ol>
 
-        <OTPRoot maxLength={6} autoFocus={true} inputMode="numeric" {onComplete} bind:value className="otp-root">
-            <OTPInput index={0} className="otp-input" focusClassName="focused" />
-            <OTPInput index={1} className="otp-input" focusClassName="focused" />
-            <OTPInput index={2} className="otp-input" focusClassName="focused" />
-            <OTPInput index={3} className="otp-input" focusClassName="focused" />
-            <OTPInput index={4} className="otp-input" focusClassName="focused" />
-            <OTPInput index={5} className="otp-input" focusClassName="focused" />
-        </OTPRoot>
+        <PinInput.Root bind:value maxlength={6} {onComplete} pattern={REGEXP_ONLY_DIGITS} class="otp-root">
+            {#snippet children({ cells })}
+                {#each cells as cell, i (i)}
+                    <PinInput.Cell {cell} class="otp-input">
+                        {#if cell.char !== null}
+                            <span>{cell.char}</span>
+                        {/if}
+                        {#if cell.hasFakeCaret}
+                            <span class="caret"></span>
+                        {/if}
+                    </PinInput.Cell>
+                {/each}
+            {/snippet}
+        </PinInput.Root>
 
         {#if errorMessage}
             <span class="error">{errorMessage}</span>
@@ -88,22 +96,40 @@
         display: flex;
         justify-content: center;
         gap: 0.25rem;
-
-        color: black;
-        font-size: 2rem;
     }
 
     :global(.otp-input) {
-        width: 1em;
-        height: 1.4em;
-
-        border: 1px rgb(80, 80, 80) solid;
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 2rem;
+        height: 2.8rem;
+        font-size: 2rem;
+        color: black;
+        border: 1px solid rgb(80, 80, 80);
         border-radius: 0.325rem;
+
+        &[data-active] {
+            border: 2px solid black;
+        }
     }
 
-    :global(.otp-input.focused) {
-        border-width: 2px;
-        border-color: black;
+    :global(.caret) {
+        position: absolute;
+        width: 2px;
+        height: 1.5rem;
+        background-color: black;
+        animation: blink 1s step-end infinite;
+    }
+
+    @keyframes blink {
+        0%, 70%, 100% {
+            opacity: 1;
+        }
+        20%, 50% {
+            opacity: 0;
+        }
     }
 
     .error {
